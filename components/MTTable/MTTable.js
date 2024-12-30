03726b60d6e1f5d79ae8b0d70dbf9f0ffb54aace
zmt <657885619@qq.com>
2024-12-30 16:42:10 +0800

    修改单元格宽度单位为px

--- components/MTTable/MTTable.js
+++ components/MTTable/MTTable.js
@@ -2,27 +2,27 @@ Component({
 	options: {
 		multipleSlots: true, // 在组件定义时的选项中启用多slot支持.
 		styleIsolation: "apply-shared",//父组件的样式会影响组件内样式
-  },
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+	},
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
 		/**
 		 * 当前存在选中框时选择框的字段
 		 */
-		chioceKeys:{
-			type:String,
-			value:"checked"
+		chioceKeys: {
+			type: String,
+			value: "checked"
 		},
-    /**
-     * 对齐方式 left center right
-     * */
-    align: {
-      type: String,
+		/**
+		 * 对齐方式 left center right
+		 * */
+		align: {
+			type: String,
 			value: 'left',
-			observer:function(newVal,oldVal){
+			observer: function (newVal, oldVal) {
 				let _align = "flex-start";
-				switch(newVal){
+				switch (newVal) {
 					case "left":
 						_align = "flex-start";
 						break;
@@ -37,72 +37,73 @@ Component({
 					_align
 				})
 			}
-    },
-    /**
-     * 指定表格最大高度
-     */
-    maxHeight:{
-      type: String,
-      value: '20'
+		},
+		/**
+		 * 指定表格最大高度
+		 */
+		maxHeight: {
+			type: String,
+			value: '20'
 		},
 		/***
 		 * 固定高度
 		 */
-		height:{
-      type: String,
-      value: ''
+		height: {
+			type: String,
+			value: ''
 		},
-    /**
-     * 是否固定左边标题
-     */
-    left:{
-      type: Boolean,
-      value: false
-    },
-    /**
-     * 斑马纹
-     */
-    stripe: {
-      type: Boolean,
-      value: true
-    },
-    /**
-     * 是否边框
-     */
-    border: {
-      type: Boolean,
-      value: false
-    },
-    /**
-     * 表头背景色
-     */
-    headbgColor: {
-      type: String,
-      value: '#f7f8fa'
-    },
-    /**
-     * 操作文本 不传不显示
-     */
-    opText: {
-      type: String,
-      value: '暂无数据'
+		/**
+		 * 是否固定左边标题
+		 */
+		left: {
+			type: Boolean,
+			value: false
 		},
-   /**
-    * 表头与表体的配置信息
-    */
-	 configurator: {
-      type: Array,
+		/**
+		 * 斑马纹
+		 */
+		stripe: {
+			type: Boolean,
+			value: true
+		},
+		/**
+		 * 是否边框
+		 */
+		border: {
+			type: Boolean,
+			value: false
+		},
+		/**
+		 * 表头背景色
+		 */
+		headbgColor: {
+			type: String,
+			value: '#f7f8fa'
+		},
+		/**
+		 * 操作文本 不传不显示
+		 */
+		opText: {
+			type: String,
+			value: '暂无数据'
+		},
+		/**
+		 * 表头与表体的配置信息
+		 */
+		configurator: {
+			type: Array,
 			value: [],
-			observer:function(newVal,oldVal){
+			observer: function (newVal, oldVal) {
 				let totalWidth = 0;
 				for (const item of newVal) {
-					totalWidth += item.width ? Number(item.width.slice(0,item.width.indexOf("r"))): 80;
+					totalWidth += item.width ? Number(item.width.slice(0, item.width.indexOf("r"))) / 2 : 80;
+					item.widthNotRpx = item.width ? (Number(item.width.slice(0, item.width.indexOf("r"))) / 2) + 'px' : '';//将对应的宽度的rpx转化为px
 				};
 				// 重置当前固定列的位置config数组
 				let config = []
-				config =  this.setFiexdColumnLoaction();
+				config = this.setFiexdColumnLoaction();
 				this.setData({
-					totalWidth:totalWidth,
+					totalWidth: totalWidth,
 					config,
 				});
 			}
@@ -110,17 +111,17 @@ Component({
 		/**
 		 * 表体数据
 		 */
-		tableData:{
-			type:Array,
-			value:[],
-			observer:function(newVal,oldVal){
+		tableData: {
+			type: Array,
+			value: [],
+			observer: function (newVal, oldVal) {
 				let checkedAll = true;//所有选择框都选择
 				let hasChcked = false;//存在未选中的选择框
 				for (const tableItem of newVal) {
-					if(tableItem[this.data.chioceKeys]){
+					if (tableItem[this.data.chioceKeys]) {
 						hasChcked = true;
 					}
-					if(!tableItem[this.data.chioceKeys]){
+					if (!tableItem[this.data.chioceKeys]) {
 						checkedAll = false;
 					}
 				};
@@ -130,95 +131,95 @@ Component({
 				})
 			}
 		},
-    /**
-     * 文字过长省略
-     */
-    showOverflowTooltip:{
-      type:Boolean,
-			value:false,
-    }
-  },
+		/**
+		 * 文字过长省略
+		 */
+		showOverflowTooltip: {
+			type: Boolean,
+			value: false,
+		}
+	},
 
 	/**
 	 * 外部样式修改
 	 */
 	externalClasses: [],
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
 		// 当前单元格汇总的宽度
-		totalWidth:0,
+		totalWidth: 0,
 		// 单元格内项目位置
-		_align:"flex-start",
+		_align: "flex-start",
 		// 判断是否勾选了所有的选择
-		checkedAll:false,
-		hasChcked:false,
+		checkedAll: false,
+		hasChcked: false,
 		// 当前渲染的高度已经超过给定高度
-		overHeight:false,
+		overHeight: false,
 		// 表头表体配置信息
-		config:[],
+		config: [],
 	},
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    /**
-     * 获取当前行数据
-     * @param {event} event 
-     */
-    _rowClick(event) {
-      //当前行数据
-			const {item} = event.currentTarget.dataset;
-      this.triggerEvent('rowClick', item);
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+		/**
+		 * 获取当前行数据
+		 * @param {event} event 
+		 */
+		_rowClick(event) {
+			//当前行数据
+			const { item } = event.currentTarget.dataset;
+			this.triggerEvent('rowClick', item);
 		},
 
 		/**
 		 * 选择所有数据
 		 */
-		_chioceAll(){
-			let {tableData,chioceKeys} = this.data;
+		_chioceAll() {
+			let { tableData, chioceKeys } = this.data;
 			// 勾选全部
-			if(this.data.checkedAll){
+			if (this.data.checkedAll) {
 				for (const tableItem of tableData) {
 					tableItem[chioceKeys] = false;
 				}
-			}else{
-			// 取消勾选全部
+			} else {
+				// 取消勾选全部
 				for (const tableItem of tableData) {
 					tableItem[chioceKeys] = true;
 				}
 			};
-			this.triggerEvent("changeChoice",tableData)
+			this.triggerEvent("changeChoice", tableData)
 		},
 		// promise---调整异步为同步(clasName:包裹表体或者表头的类名)
-		transformToPromise(clasName,attribute){
-				const _that = this;
-				const query = wx.createSelectorQuery().in(_that)
-				return new Promise((resolve,reject)=>{
-					query.select(clasName).boundingClientRect(react=>{
-						resolve(react[attribute])
-					}).exec();
-				})
+		transformToPromise(clasName, attribute) {
+			const _that = this;
+			const query = wx.createSelectorQuery().in(_that)
+			return new Promise((resolve, reject) => {
+				query.select(clasName).boundingClientRect(react => {
+					resolve(react[attribute])
+				}).exec();
+			})
 		},
 		// 如果当前的列配置了Fixed=true时，将排序顺序前移(left="左移";right="右移")
-		setFiexdColumnLoaction(){
+		setFiexdColumnLoaction() {
 			const _that = this;
 			let configArray = _that.data.configurator;
 			let configResetArray = [];
 			for (const itemValue of configArray) {
-				let index = configArray.findIndex(t=>t.prop == itemValue.prop);
-				if(itemValue?.fiexed){
+				let index = configArray.findIndex(t => t.prop == itemValue.prop);
+				if (itemValue?.fiexed) {
 					let setFixedLoaction = configResetArray.length;
 					for (const resetItem of configResetArray) {
-						if(!resetItem?.fiexed){
-							setFixedLoaction = configArray.findIndex(t=>t.prop == resetItem.prop);
+						if (!resetItem?.fiexed) {
+							setFixedLoaction = configArray.findIndex(t => t.prop == resetItem.prop);
 							break;
 						};
 					};
-					configResetArray.splice(setFixedLoaction,0,itemValue);
-				}else{
+					configResetArray.splice(setFixedLoaction, 0, itemValue);
+				} else {
 					itemValue.fiexed = false;
 					itemValue.fiexdOrder = index + 1;
 					configResetArray.push(itemValue)
@@ -227,60 +228,78 @@ Component({
 			return configResetArray;
 		},
 		// 计算当前配置信息的固定列开始初始偏移位置
-	  async	setColumnFixedMovewidth(configResetArray){	
-			 const _that = this;
-			 for (const item of configResetArray) {
-				let index = configResetArray.findIndex(t=>t.prop == item.prop);
-				if(item?.fiexed){
-					if(item?.fiexedPosition){
+		async setColumnFixedMovewidth(configResetArray) {
+			const _that = this;
+			for (const item of configResetArray) {
+				let index = configResetArray.findIndex(t => t.prop == item.prop);
+				if (item?.fiexed) {
+					if (item?.fiexedPosition) {
 						item.fiexedPosition = 'left';
 					};
-					if(index == 0){
+					if (index == 0) {
 						item.left = 0;
-					}else{
+					} else {
 						item.left = 0;
-						if(index != -1){
+						if (index != -1) {
 							// 无论是否配置宽度按都需要计算一下，固定列的
-							for(let i = 0; i<=index;i++){
-								if(i == index){
+							for (let i = 0; i <= index; i++) {
+								if (i == index) {
 									break;
 								};
-								item.left += (await _that.transformToPromise('.mt-header-column'+i,'width'));
+								item.left += (await _that.transformToPromise('.mt-header-column' + i, 'width'));
 							};
 						};
 					}
 				};
-			 }
-			 _that.setData({
-				 config:configResetArray
-			 });
+			}
+			_that.setData({
+				config: configResetArray
+			});
 		},
-  },
-  /**
-   * 监听器
-   */
-  observers: {
-  },
-  /**
-   * 生命周期
-   */
-  lifetimes:{
-    attached: function() {
+	},
+	/**
+	 * 监听器
+	 */
+	observers: {
+	},
+	/**
+	 * 生命周期
+	 */
+	lifetimes: {
+		attached: function () {
 			console.log("在组件实例进入页面节点树时执行");
-    },
-    detached: function() {
-      console.log("在组件实例被从页面节点树移除时执行");
 		},
-	  ready: async function(){
+		detached: function () {
+			console.log("在组件实例被从页面节点树移除时执行");
+		},
+		ready: async function () {
 			console.log("页面元素已经布局完成");
 			let tableContainerHeight = 0;
-			tableContainerHeight += await this.transformToPromise('.mt-header','height');
-			tableContainerHeight += await this.transformToPromise('.mt-table-tr-container','height');
+			tableContainerHeight += await this.transformToPromise('.mt-header', 'height');
+			tableContainerHeight += await this.transformToPromise('.mt-table-tr-container', 'height');
+			// 获取父容器的宽度按
+			let tableFatherContainer = await this.transformToPromise('.mt-table-scroll', 'width');
+			// 如果当前父容器的宽度大于内部元素的宽度，将对应的某列的width设置为空--解决rpx在不同机型转化后父元素内无法完全包裹
+			if (this.data.totalWidth < tableFatherContainer) {
+				let config = JSON.parse(JSON.stringify(this.data.config));
+				for (let i = 0; i < config.length; i++) {
+					if (!config[i].width) {
+						break;
+					}
+					if (i == config.length - 1) {
+						config[i].width = ""
+						config[i].widthNotRpx = ""
+						this.setData({
+							config
+						})
+					}
+				}
+			}
 			// 解决表格下边框重叠的问题
-			let overHeight =  this.data.overHeight;
-			if(tableContainerHeight > Number(this.data.height)){
+			let overHeight = this.data.overHeight;
+			if (tableContainerHeight > Number(this.data.height)) {
 				overHeight = true;
-			}else{
+			} else {
 				overHeight = false;
 			};
 			this.setData({
@@ -291,18 +310,18 @@ Component({
 		}
 	},
 
-  /**
-   * 声明周期函数
-   */
-  pageLifetimes: {
-    show: function() {
+	/**
+	 * 声明周期函数
+	 */
+	pageLifetimes: {
+		show: function () {
 
-    },
-    hide: function() {
-      // 页面被隐藏
-    },
-    resize: function(size) {
-      // 页面尺寸变化
-    }
-  }
+		},
+		hide: function () {
+			// 页面被隐藏
+		},
+		resize: function (size) {
+			// 页面尺寸变化
+		}
+	}
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,438 +1,438 @@
 // index.js
 Page({
-  data: {
+	data: {
 		// 测试--基础
-		testTableConfig:[
+		testTableConfig: [
 			{
-				prop:"Name",
-				width:"150rpx",
-				label:"姓名",
-				needPadding:true,
+				prop: "Name",
+				width: "150rpx",
+				label: "姓名",
+				needPadding: true,
 			},
 			{
-				prop:"sex",
-				width:"150rpx",
-				label:"性别",
-				needPadding:true,
+				prop: "sex",
+				width: "150rpx",
+				label: "性别",
+				needPadding: true,
 			},
 			{
-				prop:"schoolNumber",
-				width:"150rpx",
-				label:"学号",
-				needPadding:true,
+				prop: "schoolNumber",
+				width: "150rpx",
+				label: "学号",
+				needPadding: true,
 			},
 			{
-				prop:"adress",
-				width:"",
-				label:"住址",
-				needPadding:true,
+				prop: "adress",
+				width: "",
+				label: "住址",
+				needPadding: true,
 			},
 		],
 		// 测试--选择框
-		testTableConfigWithCheck:[
+		testTableConfigWithCheck: [
 			{
-				prop:"checked",
-				width:"80rpx",
-				fiexed:true,
-				fiexedPosition:"left",
-				label:"选择",
-				needPadding:true,
+				prop: "checked",
+				width: "80rpx",
+				fiexed: true,
+				fiexedPosition: "left",
+				label: "选择",
+				needPadding: true,
 			},
 			{
-				prop:"sex",
-				width:"150rpx",
-				fiexed:true,
-				fiexedPosition:"left",
-				label:"性别",
-				needPadding:true,
+				prop: "sex",
+				width: "150rpx",
+				fiexed: false,
+				fiexedPosition: "left",
+				label: "性别",
+				needPadding: true,
 			},
 			{
-				prop:"schoolNumber",
-				width:"150rpx",
-				label:"学号",
-				needPadding:true,
+				prop: "schoolNumber",
+				width: "150rpx",
+				label: "学号",
+				needPadding: true,
 			},
 			{
-				prop:"Name",
-				width:"",
-				fiexed:true,
-				fiexedPosition:"left",
-				label:"姓名",
-				needPadding:true,
+				prop: "Name",
+				width: "150rpx",
+				fiexed: false,
+				fiexedPosition: "left",
+				label: "姓名",
+				needPadding: true,
 			},
 			{
-				prop:"adress",
-				width:"150rpx",
-				label:"住址",
-				needPadding:true,
+				prop: "adress",
+				width: "150rpx",
+				label: "住址",
+				needPadding: true,
 			},
-			{
-				prop:"phonenumber",
-				width:"150rpx",
-				label:"电话",
-				needPadding:true,
-      },
-      {
-				prop:"operation",
-				width:"150rpx",
-				label:"操作",
-				needPadding:true,
-			},
+			// {
+			// 	prop: "phonenumber",
+			// 	width: "150rpx",
+			// 	label: "电话",
+			// 	needPadding: true,
+			// },
+			// {
+			// 	prop: "operation",
+			// 	width: "150rpx",
+			// 	label: "操作",
+			// 	needPadding: true,
+			// },
 		],
 		// 测试--数组内嵌套数组
-		arrayTableConfigWithCheck:[
+		arrayTableConfigWithCheck: [
 			{
-				prop:"sex",
-				width:"150rpx",
-				label:"性别",
-				needPadding:true,
+				prop: "sex",
+				width: "150rpx",
+				label: "性别",
+				needPadding: true,
 			},
 			{
-				prop:"schoolNumber",
-				width:"150rpx",
-				label:"学号",
-				needPadding:true,
+				prop: "schoolNumber",
+				width: "150rpx",
+				label: "学号",
+				needPadding: true,
 			},
 			{
-				prop:"Name",
-				width:"150rpx",
-				label:"姓名",
-				needPadding:true,
+				prop: "Name",
+				width: "150rpx",
+				label: "姓名",
+				needPadding: true,
 			},
 			{
-				prop:"adress",
-				width:"150rpx",
-				label:"住址",
-				needPadding:true,
+				prop: "adress",
+				width: "150rpx",
+				label: "住址",
+				needPadding: true,
 			},
 			{
-				prop:"testArray",
-				width:"150rpx",
-				label:"数组循环",
-        needPadding:false,
-        loopwidth:"300rpx",
-        loopArray:[
-          {
-            width:"150rpx",
-            prop:"testArray",
-          },
-          {
-            width:"150rpx",
-            prop:"testArray3",
-          },
-      ],
+				prop: "testArray",
+				width: "150rpx",
+				label: "数组循环",
+				needPadding: false,
+				loopwidth: "300rpx",
+				loopArray: [
+					{
+						width: "150rpx",
+						prop: "testArray",
+					},
+					{
+						width: "150rpx",
+						prop: "testArray3",
+					},
+				],
 			},
 			{
-				prop:"testArray3",
-				width:"150rpx",
-				label:"数组循环2",
-        needPadding:false,
-        tbodyLoop:true,
-      },
+				prop: "testArray3",
+				width: "150rpx",
+				label: "数组循环2",
+				needPadding: false,
+				tbodyLoop: true,
+			},
 		],
-		tableData:[
+		tableData: [
 			{
-				Name:"小张",
-        sex:"男",
-        schoolNumber:1,
-				checked:false,
-        id:1,
-        adress:"浙江省XXX市",
-        phonenumber:"132XXXXX4578",
-				testArray:[
+				Name: "小张",
+				sex: "男",
+				schoolNumber: 1,
+				checked: false,
+				id: 1,
+				adress: "浙江省XXX市",
+				phonenumber: "132XXXXX4578",
+				testArray: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				],
-				testArray3:[
+				testArray3: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				]
 			},
 			{
-				Name:"小张",
-        sex:"男",
-        schoolNumber:1,
-				checked:false,
-        id:12,
-        adress:"浙江省XXX市",
-        phonenumber:"132XXXXX4578",
-				testArray:[
+				Name: "小张",
+				sex: "男",
+				schoolNumber: 1,
+				checked: false,
+				id: 12,
+				adress: "浙江省XXX市",
+				phonenumber: "132XXXXX4578",
+				testArray: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				],
-				testArray3:[
+				testArray3: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				]
-      },
-      {
-				Name:"小张",
-        sex:"男",
-        schoolNumber:1,
-				checked:false,
-        id:13,
-        adress:"浙江省XXX市",
-        phonenumber:"132XXXXX4578",
-				testArray:[
+			},
+			{
+				Name: "小张",
+				sex: "男",
+				schoolNumber: 1,
+				checked: false,
+				id: 13,
+				adress: "浙江省XXX市",
+				phonenumber: "132XXXXX4578",
+				testArray: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				],
-				testArray3:[
+				testArray3: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				]
-      },
-      {
-				Name:"小张",
-        sex:"男",
-        schoolNumber:1,
-				checked:false,
-        id:14,
-        adress:"浙江省XXX市",
-        phonenumber:"132XXXXX4578",
-				testArray:[
+			},
+			{
+				Name: "小张",
+				sex: "男",
+				schoolNumber: 1,
+				checked: false,
+				id: 14,
+				adress: "浙江省XXX市",
+				phonenumber: "132XXXXX4578",
+				testArray: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				],
-				testArray3:[
+				testArray3: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				]
-      },
-      {
-				Name:"小张",
-        sex:"男",
-        schoolNumber:1,
-				checked:false,
-        id:15,
-        adress:"浙江省XXX市",
-        phonenumber:"132XXXXX4578",
-				testArray:[
+			},
+			{
+				Name: "小张",
+				sex: "男",
+				schoolNumber: 1,
+				checked: false,
+				id: 15,
+				adress: "浙江省XXX市",
+				phonenumber: "132XXXXX4578",
+				testArray: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
-					},
-				],
-				testArray3:[
-					{
-					name:"11",
-					label:"cc",
-					},
-					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				],
-      },
-      {
-				Name:"小张",
-        sex:"男",
-        schoolNumber:1,
-				checked:false,
-        id:16,
-        adress:"浙江省XXX市",
-        phonenumber:"132XXXXX4578",
-				testArray:[
+				testArray3: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				],
-				testArray3:[
+			},
+			{
+				Name: "小张",
+				sex: "男",
+				schoolNumber: 1,
+				checked: false,
+				id: 16,
+				adress: "浙江省XXX市",
+				phonenumber: "132XXXXX4578",
+				testArray: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
+					},
+				],
+				testArray3: [
+					{
+						name: "11",
+						label: "cc",
+					},
+					{
+						name: "11",
+						label: "cc",
 					},
 				]
-      },
-      {
-				Name:"小张",
-        sex:"男",
-        schoolNumber:1,
-				checked:false,
-        id:17,
-        adress:"浙江省XXX市",
-        phonenumber:"132XXXXX4578",
-				testArray:[
+			},
+			{
+				Name: "小张",
+				sex: "男",
+				schoolNumber: 1,
+				checked: false,
+				id: 17,
+				adress: "浙江省XXX市",
+				phonenumber: "132XXXXX4578",
+				testArray: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				],
-				testArray3:[
+				testArray3: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				]
-      },	
-      {
-				Name:"小张",
-        sex:"男",
-        schoolNumber:1,
-				checked:false,
-        id:18,
-        adress:"浙江省XXX市",
-        phonenumber:"132XXXXX4578",
-				testArray:[
+			},
+			{
+				Name: "小张",
+				sex: "男",
+				schoolNumber: 1,
+				checked: false,
+				id: 18,
+				adress: "浙江省XXX市",
+				phonenumber: "132XXXXX4578",
+				testArray: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				],
-				testArray3:[
+				testArray3: [
 					{
-					name:"111111111111111111111111111111",
-					label:"cc11111",
+						name: "111111111111111111111111111111",
+						label: "cc11111",
 					},
 					{
-						name:"1111111111111111111111111111111111111111111",
-						label:"cc",
+						name: "1111111111111111111111111111111111111111111",
+						label: "cc",
 					},
 				]
-      },
-      {
-				Name:"小张",
-        sex:"男",
-        schoolNumber:1,
-				checked:false,
-        id:19,
-        adress:"浙江省XXX市",
-        phonenumber:"132XXXXX4578",
-				testArray:[
+			},
+			{
+				Name: "小张",
+				sex: "男",
+				schoolNumber: 1,
+				checked: false,
+				id: 19,
+				adress: "浙江省XXX市",
+				phonenumber: "132XXXXX4578",
+				testArray: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				],
-				testArray3:[
+				testArray3: [
 					{
-					name:"11",
-					label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 					{
-						name:"11",
-						label:"cc",
+						name: "11",
+						label: "cc",
 					},
 				]
 			},
 		]
 	},
 	// 选择框点击事件
-	checked(e){
-			let {checked}= e.currentTarget.dataset;
-			let index = this.data.tableData.findIndex(t=>t.id == checked.id);
-			let tableData = this.data.tableData;
-			if(index != -1){
-				tableData[index].checked = 	!tableData[index].checked;
-				this.setData({
-					tableData
-				})
-			}
+	checked(e) {
+		let { checked } = e.currentTarget.dataset;
+		let index = this.data.tableData.findIndex(t => t.id == checked.id);
+		let tableData = this.data.tableData;
+		if (index != -1) {
+			tableData[index].checked = !tableData[index].checked;
+			this.setData({
+				tableData
+			})
+		}
 	},
 	// 选择框全选--请将所有选择数据塞入要勇于勾选的数据
-	changeChoice(e){
+	changeChoice(e) {
 		this.setData({
-			tableData:e.detail
+			tableData: e.detail
 		})
 	},
 	// 子组件返回点击事件数据
-	rowClick(e){
+	rowClick(e) {
 		console.log(e.detail);
-  },
-  opration(e){
-    console.log(e.currentTarget.dataset.row,"操作列点击");
-  }
+	},
+	opration(e) {
+		console.log(e.currentTarget.dataset.row, "操作列点击");
+	}
 })
